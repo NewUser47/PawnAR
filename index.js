7ea28d4050e0afd0dfddd7ccb5c3772780e9854a
ApoorvAgrawal <66911709+NewUser47@users.noreply.github.com>
2021-09-19 09:25:47 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,6 @@
+// adds 'active' class to the container when signin or signup is clicked
+function toggleForm(){
+    var container= document.querySelector('.container');
+    container.classList.toggle('active');
+    }
+    
